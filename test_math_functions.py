a07b083cea098a644db9bcc790bf3e8d877501eb
root <root@DESKTOP-36AKIJI.localdomain>
2022-11-28 13:36:42 -0500

    Basic python math functions and tests

---
+++ test_math_functions.py
@@ -0,0 +1,21 @@
+from math_functions import *
+
+def testAdd():
+    output = add_numbers(2,4)
+    assert output == 6
+
+def testSub():
+    output = subtract_numbers(2,4)
+    assert output == -2
+
+def testMult():
+    output = multiply_numbers(2,4)
+    assert output == 8
+
+def testMultFail():
+    output = multiply_numbers(2,4)
+    assert output == 16
+
+def testDiv():
+    output = divide_numbers(10,2)
+    assert output == 5
